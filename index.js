12f80f3153c110783a7a2df9904f015dc47bbabf
Niladri-Gudu <niladrigudu@gmail.com>
2025-05-20 09:55:44 +0530

    added frontend url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 app.use(morgan('dev'));
