0d6d99789711de7dffec2cfd5b31b42b15ee1240
Niladri-Gudu <niladrigudu@gmail.com>
2025-05-26 19:51:08 +0530

    fixinggg

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: [`${process.env.FRONTEND_URL}`, 'http://localhost:3000'], // Add your production frontend URL
+  origin: [`${process.env.FRONTEND_URL}`, 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
